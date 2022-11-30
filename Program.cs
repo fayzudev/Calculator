58e4bffdbb1980a5ebd199e5aa62e4186904f62f
frakhimov505 <87068169+frakhimov505@users.noreply.github.com>
2022-11-30 12:31:01 +0500

    upload

--- Program.cs
+++ Program.cs
@@ -1,1 +1,64 @@
-﻿
+﻿System.Console.WriteLine("Here I Started a calculator program");
+System.Console.Write("1-sonni kiriting: ");
+int a = int.Parse(System.Console.ReadLine());
+System.Console.Write("2-sonni kiriting: ");
+int b = int.Parse(System.Console.ReadLine());
+System.Console.Write("Amallardan birini tanlang(+,-,*,/,%): ");
+string operation = Console.ReadLine();
+
+if(a>b)
+{
+    System.Console.WriteLine($"{a} + {b} = {a+b}");
+}
+else if(a<b)
+{
+    System.Console.WriteLine($"{a} - {b} = {a-b}");
+}
+else
+{System.Console.WriteLine("Opertion not found");}
+
+string message=
+    a>=b
+        ? "saadsad"
+        : "dsada";
+System.Console.WriteLine(message);
+
+// switch(operation)
+// {
+//     case "+":
+//         Console.WriteLine($"{a} + {b} = {a+b}");
+//         break;
+
+//     case "-":
+//         Console.WriteLine($"{a} - {b} = {a-b}");
+//         break;
+
+//     case "*":
+//         Console.WriteLine($"{a} * {b} = {a*b}");
+//         break;
+
+//     case "/":
+//         Console.WriteLine($"{a} / {b} = {a/b}");
+//         break;
+
+//     case "%":
+//         Console.WriteLine($"{a} % {b} = {a%b}");
+//         break;
+
+//     default:
+//         System.Console.WriteLine("Opertion not found");
+//         break;
+// }
+
+string result = operation switch
+{
+    "+" => $"{a} + {b} = {a+b}",
+    "-" => $"{a} - {b} = {a-b}",
+    "*" => $"{a} * {b} = {a*b}",
+    "/" => $"{a} / {b} = {a/b}",
+    "%" => $"{a} % {b} = {a%b}",
+    _   => "Operation not found"
+
+};
+System.Console.WriteLine(result);
+
